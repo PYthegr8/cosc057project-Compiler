92d5fe3b96c5a624da64ced8e3eab3959f1c5667
Papa Yaw <papayawowusunti@gmail.com>
2026-03-12 11:17:22 -0400

    updating the max_n test

--- compiler_backend/assembly_gen_tests/max_n.c
+++ compiler_backend/assembly_gen_tests/max_n.c
@@ -2,17 +2,19 @@ extern void print(int);
 extern int read();
 
 int func(int n){
-	int max = 0;
+	int max;
 	int i;
 	int a;
+	max = 0;
 	i = 0;
-	
-	while (i < n){ 
+
+	while (i < n){
 		a = read();
 		if (a > max)
 			max = a;
 		i = i + 1;
 	}
-	
+
 	return max;
 }
+
